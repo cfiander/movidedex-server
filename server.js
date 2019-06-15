8f3dde3cce564714871ac2ebc6acd3740c9ed6f7
cfiander <cfiander90@gmail.com>
2019-06-15 14:31:22 -0700

    initial

---
+++ server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies-data-small.json');
+
+const app = express();
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(cors())
+app.use(helmet())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  //move to the next middleware
+  next();
+});
+
+app.get('/movie', function handleGetMovie(req, res) {
+  const { genre = '', country = '', avg_vote } = req.query;
+
+  let results = MOVIES;
+  if (genre) {
+    results = results.filter(movie => {
+      return movie.genre.toLowerCase().includes(genre.toLowerCase());
+    });
+  }
+  //console.log(results);
+  if (country) {
+    results = results.filter(movie => {
+      return movie.country.toLowerCase().includes(country.toLowerCase());
+    });
+  }
+  //console.log(results);
+  if (avg_vote) {
+    results = results.filter(movie => {
+      return parseFloat(movie.avg_vote) >= parseFloat(avg_vote);
+    });
+  }
+  //console.log(results);
+  res.json(results);
+});
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  //console.log(`Server listening at http://localhost:${PORT}`)
+})
